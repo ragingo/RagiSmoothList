e46ef86118ba29ae49299a4a6a2e9df0c4b91a33
ragingo <save.the.world.crush@gmail.com>
2022-12-08 16:59:46 +0900

    setup

---
+++ Sources/RagiSmoothList/RagiSmoothList.swift
@@ -0,0 +1,20 @@
+//
+//  RagiSmoothList.swift
+//  RagiSmoothList
+//
+//  Created by ragingo on 2022/12/08.
+//
+
+import SwiftUI
+
+public struct RagiSmoothList: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RagiSmoothList_Previews: PreviewProvider {
+    static var previews: some View {
+        RagiSmoothList()
+    }
+}
