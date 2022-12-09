cecbcb4f6abcc4b5c657ba7be62fec7ca01d8451
ragingo <save.the.world.crush@gmail.com>
2022-12-09 20:53:53 +0900

    InnerTableViewCell から UIHostingController に関する実装を分離

--- Sources/RagiSmoothList/InnerTableViewCell.swift
+++ Sources/RagiSmoothList/InnerTableViewCell.swift
@@ -5,41 +5,15 @@
 //  Created by ragingo on 2022/12/08.
 //
 
-// original: https://github.com/noahsark769/NGSwiftUITableCellSizing/blob/main/NGSwiftUITableCellSizing/HostingCell.swift
-// license:  https://github.com/noahsark769/NGSwiftUITableCellSizing/blob/c346e6c/LICENSE
-/**
- MIT License
-
- Copyright (c) 2020 Noah Gilmore
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
 
 import SwiftUI
 import UIKit
 
 final class InnerTableViewCell<Content: View>: UITableViewCell {
-    private let hostingController = UIHostingController<Content?>(rootView: nil)
+    private var hostingView = HostingView<Content>()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        hostingController.view.backgroundColor = .clear
     }
 
     required init?(coder decoder: NSCoder) {
@@ -47,25 +21,16 @@ final class InnerTableViewCell<Content: View>: UITableViewCell {
     }
 
     func set(content: Content, parentController: UIViewController?) {
-        hostingController.rootView = content
-        hostingController.view.invalidateIntrinsicContentSize()
+        hostingView.set(content: content, parentController: parentController)
+        hostingView.invalidateIntrinsicContentSize()
 
-        let requiresControllerMove = hostingController.parent != parentController
-        if requiresControllerMove {
-            parentController?.addChild(hostingController)
-        }
-
-        if !contentView.subviews.contains(hostingController.view) {
-            contentView.addSubview(hostingController.view)
-            hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-            hostingController.view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-            hostingController.view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-            hostingController.view.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-            hostingController.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        }
-
-        if requiresControllerMove {
-            hostingController.didMove(toParent: parentController)
+        if !subviews.contains(hostingView) {
+            addSubview(hostingView)
+            hostingView.translatesAutoresizingMaskIntoConstraints = false
+            hostingView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            hostingView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            hostingView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            hostingView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         }
     }
 }
